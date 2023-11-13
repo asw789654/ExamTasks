01653d3ff162e122cc78b1646085111f7f521c88
iliuha135@gmail.com <iliuha135@gmail.com>
2023-11-13 21:50:49 +0300

    Добавьте файлы проекта.

---
+++ ExamTask1/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace ExamTask1
+{
+    internal class MyStack
+    {
+        private int[] numbers = new int[0];
+
+        public MyStack(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public int[] Numbers 
+        {
+            get { return numbers; }
+            set { numbers = value; }
+        }
+
+        public int pop()
+        {
+            int number = numbers[numbers.Length - 1];
+            int[] newnumbers = new int[numbers.Length - 1];
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                newnumbers[i] = numbers[i];
+            }
+            numbers = newnumbers;
+            return number;
+        }
+
+        public void push(int number)
+        {
+            int[] newnumbers = new int[numbers.Length + 1];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newnumbers[i] = numbers[i];
+            }
+            numbers = newnumbers;
+            numbers[numbers.Length - 1] = number;
+            Console.WriteLine($"Добавлен элемент {number}");
+        }
+    }
+}
---
+++ ExamTask1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExamTask1;
+
+int[] numbers = { 1, 2, 3 };
+MyStack myStack = new MyStack(numbers);
+//myStack.push(2);
+myStack.pop();
+for (int i = 0; i < myStack.Numbers.Length; i++)
+{
+    Console.WriteLine(myStack.Numbers[i]);
+}
